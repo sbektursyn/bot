829b882686a909e3a3567696a611b77bda7767de
Sagyndyk <sagyndyk@MacBook-Pro-Sagyndyk.local>
2023-01-05 21:10:38 +0600

    full

--- bot.js
+++ bot.js
@@ -3,11 +3,18 @@ const input=document.querySelector("#myText"),
 	  sms= document.querySelector(".sms"),
 	  wrapper=document.querySelector(".section");
 const myText = ["Hello","How are you?","Goodbye"]
-const botText = ["Hi","Good","Gbye"]
+const botText = ["Hi","Good You?","Gbye"]
+
+
+input.addEventListener("keyup", function(event) {
+	if (event.keyCode === 13) {
+    	document.getElementById("btn").click();
+	}
+});
 
 function send(){
 	const text = input.value;
-	console.log(input);
+	console.log(text);
 	if(text!=""){
 		sms.innerHTML +=`<div class="smswrapper"><p class="text1">${text} </p></div>`
 		
@@ -15,24 +22,36 @@ function send(){
 		
 
 		for (var i = 0; i < myText.length; i++) {
-		    if (myText[i] == text) {
+			if (myText[i] == text) {
 		        let mysms = myText.indexOf(text)
 		        console.log(mysms)
 
 		        console.log(botText[mysms])
 
-		        sms.innerHTML += '<div class="botsms"><p class="btext" id="btext"></p></div>';
-		        document.getElementById("btext").innerHTML = botText[mysms];
-		    } else if (text != myText[i]) {
-		    	sms.innerHTML += '<div class="botsms"><p class="btext">не понимае</p></div>';
+
+		        sms.innerHTML += `<div class="botsms"><p class="btext"> ${botText[mysms]} </p></div>`;
+		        input.value=''
+		        return
+		    } 
+		    if (myText[0] != text){
+		    	if (myText[1] != text){
+		    		if (myText[2] != text){
+		    			sms.innerHTML += '<div class="botsms"><p class="btext">не понимае</p></div>';
+		    			input.value=''
+		    			return
+		    		}
+		    	}
 		    }
 		}
 
 
-
-		input.value=''
+		
+		
+		
 	}
 }
+
+
 
 
 
